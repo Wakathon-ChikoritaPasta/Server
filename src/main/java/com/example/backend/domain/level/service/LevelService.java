85a79143d0d6979527f4832e1c7894bc24d12f2d
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-08-19 00:38:30 +0900

    Remove unnecessary import

--- src/main/java/com/example/backend/domain/level/service/LevelService.java
+++ src/main/java/com/example/backend/domain/level/service/LevelService.java
@@ -1,20 +1,11 @@
 package com.example.backend.domain.level.service;
 
 import com.example.backend.domain.ladybug.domain.LadybugType;
-import com.example.backend.domain.level.domain.Level;
 import com.example.backend.domain.level.dto.res.BaseIndividualRankResponseDto;
-import com.example.backend.domain.level.dto.res.BaseLevelResponseDto;
 import com.example.backend.domain.level.dto.res.IndividualRankResponseDto;
-import com.example.backend.domain.level.repository.LevelRepository;
-import com.example.backend.domain.major.domain.Major;
-import com.example.backend.domain.major.dto.res.BaseMajorRankResponseDto;
-import com.example.backend.domain.major.repository.MajorRepository;
-import com.example.backend.domain.symbol.domain.SymbolType;
 import com.example.backend.domain.user.domain.User;
 import com.example.backend.domain.user.repository.UserRepository;
-import com.example.backend.global.enums.MajorType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
