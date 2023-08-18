62d9c255036b8a9f675112a23a10dbec78b1e603
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-19 06:52:57 +0900

    [refactor] 코드정리

--- src/main/java/com/example/backend/domain/ladybug/domain/LadybugType.java
+++ src/main/java/com/example/backend/domain/ladybug/domain/LadybugType.java
@@ -6,12 +6,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum LadybugType {
-    EGG,
-    PROSPECTIVE_FRESHMAN,
-    FRESHMAN,
-    SECOND_GRADE,
-    THIRD_GRADE,
-    FORTH_GRADE;
+    EGG("봉인된 무당이"),
+    PROSPECTIVE_FRESHMAN("예비 신입생 무당이"),
+    FRESHMAN("새내기 무당이"),
+    SECOND_GRADE("2학년 무당이"),
+    THIRD_GRADE("3학년 무당이"),
+    FORTH_GRADE("4학년 무당이");
+
+    private final String stringMajorType;
+
+    public String toString() {
+        return stringMajorType;
+    }
 
     public static LadybugType updateLadybugType(LadybugType currentLadybugType) {
         LadybugType[] types = LadybugType.values();
